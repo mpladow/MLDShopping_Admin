3bf21febae42e13aba469d31c5911d58bdf2fe9a
Michael La Dow <mpladow@gmail.com>
2020-04-29 23:38:32 +1000

    SSA-21 Updated to AdminLTE 3

--- MLDShopping-Admin/Startup.cs
+++ MLDShopping-Admin/Startup.cs
@@ -58,6 +58,7 @@ namespace MLDShopping_Admin
             IMapper mapper = mappingConfig.CreateMapper();
             services.AddSingleton(mapper);
             services.AddScoped<IAuthentication, Authentication>();
+            services.AddControllersWithViews().AddRazorRuntimeCompilation();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
